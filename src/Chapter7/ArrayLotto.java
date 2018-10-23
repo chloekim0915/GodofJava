9c24a0cfa12a366f4e50e1b1a7edfbc76a194d73
chloe <chloekim0915@naver.com>
2018-10-24 05:38:50 +0900

    Chapter7 실습

---
+++ src/Chapter7/ArrayLotto.java
@@ -0,0 +1,19 @@
+package Chapter7;
+
+public class ArrayLotto {
+    public static void main(String[] args) {
+        ArrayLotto array = new ArrayLotto();
+    }
+        public void init(){
+            int[] lottoNumbers = new int[7];
+            lottoNumbers[0] =5;
+            lottoNumbers[1] =5;
+            lottoNumbers[2] =5;
+            lottoNumbers[3] =5;
+            lottoNumbers[4] =5;
+            lottoNumbers[5] =5;
+            lottoNumbers[6] =5;
+            
+        }
+    }
+
